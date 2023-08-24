5be022e933a1979f9d102416177df5a7c424b1a3
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2023-08-24 09:15:00 +0300

    Created UserRepo

---
+++ market/data_access/repositories/user_repository.py
@@ -0,0 +1,36 @@
+from market.data_access.models.user import User
+from market.data_access.repositories.IRepository import IRepository
+
+
+class UserRepository(IRepository):
+    def __init__(self, db):
+        self.db = db
+
+    def find_by_username(self, username: str) -> User:
+        return User.query.filter_by(username=username).first()
+
+    def find_by_email(self, email: str) -> User:
+        return User.query.filter_by(email_address= email).first()
+
+    def create(self, username: str, email_address: str, password: str) -> User:
+        user_to_create = User(username=username,
+                              email_address=email_address,
+                              password=password)
+        self.db.session.add(user_to_create)
+        return user_to_create
+
+    def find_all(self):
+        pass
+
+    def find_all_pageable(self, page, rows_per_page: int = 5):
+        pass
+
+    def find_by_id(self, item_id: int):
+        pass
+
+    def update(self, item):
+        pass
+
+    def delete(self, item_id: int):
+        pass
+    
