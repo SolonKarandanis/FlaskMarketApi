53f8c19a78204845a1c352bf78478e848fd50b67
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2023-08-24 09:00:06 +0300

    Added Config

--- market/__init__.py
+++ market/__init__.py
@@ -1,3 +1,29 @@
+import logging
+import os
+from logging.handlers import RotatingFileHandler
+
 from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+
+from market.config import Config
 
 app = Flask(__name__)
+logging.basicConfig(format='[%(asctime)s] %(levelname)s %(name)s: %(message)s')
+logging.getLogger().setLevel(logging.DEBUG)
+logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/flask_market.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('FlaskMarket startup')
---
+++ market/config.py
@@ -0,0 +1,7 @@
+import os
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'ec9439cfc6c796ae2029594d'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') \
+                              or "postgresql://sales_db:sales_db@192.168.1.5:5432/sales_db"
