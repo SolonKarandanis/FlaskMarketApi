3c3c84ab8f5c9fc082b166939b0a37bdb24075ec
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2023-08-25 08:12:39 +0300

    Minor change

--- src/__init__.py
+++ src/__init__.py
@@ -12,17 +12,17 @@ from src.config import Config
 
 
 def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
+    created_app = Flask(__name__, instance_relative_config=True)
     logging.basicConfig(format='[%(asctime)s] %(levelname)s %(name)s: %(message)s')
     logging.getLogger().setLevel(logging.DEBUG)
     logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 
     if test_config is None:
-        app.config.from_object(Config)
+        created_app.config.from_object(Config)
     else:
-        app.config.from_mapping(test_config)
+        created_app.config.from_mapping(test_config)
 
-    if not app.debug:
+    if not created_app.debug:
         if not os.path.exists('logs'):
             os.mkdir('logs')
         file_handler = RotatingFileHandler('logs/flask_market.log', maxBytes=10240,
@@ -30,12 +30,12 @@ def create_app(test_config=None):
         file_handler.setFormatter(logging.Formatter(
             '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
         file_handler.setLevel(logging.INFO)
-        app.logger.addHandler(file_handler)
+        created_app.logger.addHandler(file_handler)
 
-        app.logger.setLevel(logging.INFO)
-        app.logger.info('FlaskMarket startup')
+        created_app.logger.setLevel(logging.INFO)
+        created_app.logger.info('FlaskMarket startup')
 
-    return app
+    return created_app
 
 
 app = create_app()
