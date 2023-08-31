1fd77d7b7f3f12c30e038332b6a4b471ce9f90f3
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2023-08-31 09:00:25 +0300

    Added find_by_ids

--- src/data_access/repositories/product_repository.py
+++ src/data_access/repositories/product_repository.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from src.data_access.models.models import Product
 from src.data_access.repositories.IRepository import IRepository
 
@@ -9,6 +11,9 @@ class ProductRepository(IRepository):
     def find_by_id(self, product_id: int) -> Product:
         return self.db.session.query(Product)\
             .options(self.db.joinedload(Product.types)).filter_by(id=product_id).first()
+
+    def find_by_ids(self, product_ids: List[int]) -> List[Product]:
+        return self.db.session.query(Product).filter(Product.id.in_(product_ids)).all()
 
     def find_all_pageable(self, page, rows_per_page: int = 5):
         return self.db.session.query(Product).order_by(Product.id)\
