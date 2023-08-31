d21ba5ed817e797aa67135b75ed8855fc4646754
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2023-08-31 07:19:31 +0300

    Added cli commands

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
-from src import create_app
+from src import create_app, cli
 
 # Checks if the run.py file has executed directly and not imported
 if __name__ == '__main__':
     app = create_app()
+    cli.register(app)
     app.run(host="localhost", port=8000)
