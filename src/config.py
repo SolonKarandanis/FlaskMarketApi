748e06080a6c4f691433acadbf8b051702648645
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-08-26 19:34:28 +0300

    small changes

--- src/config.py
+++ src/config.py
@@ -3,6 +3,8 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'ec9439cfc6c796ae2029594d'
+    FLASK_ENV = os.environ.get('FLASK_ENV')
+    DEBUG = os.environ.get('DEBUG')
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'ec9439cfc6c796ae2029594d'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') \
                               or "postgresql://sales_db:sales_db@192.168.1.5:5432/sales_db"
@@ -35,8 +37,9 @@ class Config(object):
 
 
 class DevelopmentConfig(Config):
-    pass
+    DEBUG = True
 
 
 class ProductionConfig(Config):
-    pass
+    FLASK_ENV = 'production'
+    DEBUG = False
