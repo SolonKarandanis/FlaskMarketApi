4c4a0a8b7126416b7ac034cba0cd0d48441a6062
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-08-26 19:45:03 +0300

    Small changes

--- src/config.py
+++ src/config.py
@@ -4,7 +4,6 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'ec9439cfc6c796ae2029594d'
     FLASK_ENV = os.environ.get('FLASK_ENV')
-    DEBUG = os.environ.get('DEBUG')
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'ec9439cfc6c796ae2029594d'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') \
                               or "postgresql://sales_db:sales_db@192.168.1.5:5432/sales_db"
